bd49c9faee525d52a83182ca513c0b927c84a2ad
enpeng xu <avalongl@mmsn.com>
2012-07-11 22:09:00 -0600

    Change example projects

--- examples/client/client.cpp
+++ examples/client/client.cpp
@@ -1,49 +1,53 @@
 // client.cpp : Defines the entry point for the console application.
 //
+
 #include "sglib/iocp/iocp_client.h"
 #include "sglib/iocp/iocp_loop.h"
-#include "sglib/thread/sglib_thread.h"
 
 #include <windows.h>
-#define sleep   //Sleep(10)
+#define sleep   //Sleep(1)
 
-#define RECV_BUF_LEN  512
+
+#define CLIENT_COUNT  2
+#define COPY_COUNT    2
+
+#define RECV_BUF_LEN  (1024*64+100)
 
 typedef struct {
+	int pos;
+	int count;
 	int id;
-	char * buf;
-	int    buf_len;
-	int    recv_count;
+	char reply[2];
 	FILE * pf;
+	char buf[RECV_BUF_LEN];
+	int  recv_count;
 }session_data_t;
 
-class client_loop : public sglib::net::iocp_loop<client_loop>
-{
-	friend class sglib::net::iocp_loop<client_loop>;
-protected:
 
+class logic_loop : public sglib::net::iocp_loop<logic_loop>
+{
+	friend class sglib::net::iocp_loop<logic_loop>;
+protected:
 	void on_connected() {
 		session_data_t * sdata = new session_data_t;
 		memset(sdata, 0, sizeof(session_data_t));
-		sdata->id = _session->get_id();
-		sdata->buf_len = RECV_BUF_LEN;
-		sdata->buf = new char [sdata->buf_len];
+		sdata->id = connections();
 
 		std::cout << "\nsession["<< sdata->id<<"] NET_connected";
 
 		char path[128];
-		sprintf(path, "test\\client_session_%d.txt", sdata->id);
-		sdata->pf = fopen(path, "w");
+		sprintf(path, "test\\client_session_%d_%d.jpg", sdata->id, sdata->count);
+		sdata->pf = fopen(path, "wb");
 		_session->set_session_data(sdata);
 		_session->recv(sdata->buf, RECV_BUF_LEN);
 		sleep;
 	}
 	void on_disconnected() {
 		session_data_t * sdata = (session_data_t *)_session->get_session_data();
-		std::cout << "\nsession["<< sdata->id <<"] NET_disconnected";
-		if(sdata->pf)   // too many files opened, maybe failed
-			fclose(sdata->pf);
 
+		std::cout << "\nsession["<< sdata->id <<"] NET_disconnected";
+		if(sdata->pf)
+			fclose(sdata->pf);
 		delete sdata;
 		delete _session; //!!!
 
@@ -52,34 +56,53 @@ protected:
 		sleep;
 	}
 	void on_send_finished() {
-		assert(0);
-	}
+		session_data_t * sdata = (session_data_t *)_session->get_session_data();
 
+		std::cout << "\nsession["<< sdata->id <<"] NET_send_finished";
+		
+		sdata->recv_count = 0;
+		_session->recv(sdata->buf, RECV_BUF_LEN);
+		sleep;
+	}
 	void on_recv_finished() {
 		session_data_t * sdata = (session_data_t *)_session->get_session_data();
+		
 		std::cout << "\nsession["<< sdata->id <<"] NET_recv_finished";
-			
-		if(sdata->pf)   // too many files opened, maybe failed
-			fwrite(sdata->buf, 1, _pack.pack_size, sdata->pf);
-		if (strcmp(sdata->buf, "quit") == 0)
-			quit_cur_session(); //_session->quit();
-		else {//continue receive
-			_session->recv(sdata->buf, sdata->buf_len);
-			sleep;	
+		
+		if(_pack.pack_size == 1){
+			if(sdata->pf)
+				fclose(sdata->pf);
+			++sdata->count;
+			if(sdata->count < COPY_COUNT){
+				char tmp[128];
+				sprintf(tmp, "test\\client_session_%d_%d.jpg", sdata->id, sdata->count);
+				sdata->pf = fopen(tmp, "wb");
+				sdata->pos = 0;
+			}
+			else {
+				quit_cur_session(); //_session->quit();
+				return ;
+			}
 		}
+		else{
+			if(sdata->pf)   // too many files opened, maybe failed
+				fwrite(sdata->buf, 1, _pack.pack_size, sdata->pf);
+		}
+		
+		sdata->reply[0] = 'o';
+		sdata->reply[1] = 'k';
+		_session->send(sdata->reply, 2);
+		sleep;	
 	}
-	void on_recv_failed(){
-		// buf's size is not enough!
-		session_data_t * sdata = (session_data_t *)_session->get_session_data();
-		int pack_size = _pack.pack_size;
+	void on_recv_failed() {
 
-		ASSERT(pack_size > sdata->buf_len);
-		sdata->buf_len = pack_size * 2;
-		delete []sdata->buf;
-		sdata->buf = new char[sdata->buf_len] ;
-		_session->recv(sdata->buf, sdata->buf_len);
 	}
+	void on_noop(){
 
+	}
+	void on_protocal_error(){
+		
+	}
 	void on_send_error() {
 		std::cout << "\nsession["<< _session->get_id()<<"] NET_send_error";
 		sleep;
@@ -87,12 +110,6 @@ protected:
 	void on_recv_error() {
 		std::cout << "\nsession["<< _session->get_id()<<"] NET_recv_error";
 		sleep;
-	}
-	void on_protocal_error(){
-		std::cout << "\n ******* session["<< _session->get_id()<<"] NET_protocol_error ******";
-		sleep;
-	}
-	void on_noop(){
 	}
 	void on_unhandled() {
 		std::cout << "\nsession["<< _session->get_id()<<"] **** NET_unhandled ! ****";		
@@ -102,42 +119,45 @@ protected:
 
 int main(int argc, char * argv[])
 {
-	int port = 5001;
+	int port = 5000;
 
 	//   create the package queue first. 
-	//   server or clients will create io threads to read or write data from the queue.
+	//   the server or clients will create io threads to read or write the queue.
 	//   the loop also read & remove items from the queue.
-	//   server and client are the producer of the queue
+	//   so the server or client is the producer of the queue
 	//   the loop is the consumer of the queue
-	//   so the queue should be created first and be deleted at last! 
+	//   so the queue should be deleted at last! created first!
 	sglib::net::msg_queue::init();
+	sglib::net::iocp_client client( "127.0.0.1"
+								  , port
+								  , RECV_BUF_LEN    // send buffer size
+								  , RECV_BUF_LEN);	// send buffer size
+	
+	if (!client.open()) { 
+		client.close();	
+		return 0;
+	}
+	if(false== client.sessions_open(CLIENT_COUNT)){
+		client.close();	
+		return 0;
+	}
 
-	sglib::net::iocp_client client("127.0.0.1", port);
-	client_loop loop;
+	logic_loop loop;
 	loop.init();
 
-	if (!client.open()) { // 5000 clients
-		client.close();	
-		return 0;
-	}
-	if(false== client.sessions_open(1)){
-		client.close();	
-		return 0;
-	}
-
 	int count = 0;
-	int quit  = 0;
-
+	int quit = 0;
 	while(true){
 		if(loop.isquit())
 			break;
+
 		if(!loop.peek()){
 			continue;
 		}
 		if(!loop.tick())
 			break;
 	}
-	sglib::net::msg_queue::get_singleton()->quit(); // release consumer & producer threads.
+	sglib::net::msg_queue::get_singleton()->quit(); 
 	client.close();
 	sglib::net::msg_queue::get_singleton()->deref();
 	return 0;
--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -1,47 +1,81 @@
-/*
-	 server.cpp : 
-	This demo shows:
-	Server send data to client continuely, 
-	Client recev data and save it to hard disk.
-
-*/
+// server.cpp : Defines the entry point for the console application.
+//
 
 #include <iostream>
 #include "sglib/iocp/iocp_server.h"
 #include "sglib/iocp/iocp_loop.h"
-#include "sglib/thread/sglib_thread.h"
+//#include <boost/thread/thread.hpp>
+//#include <windows.h>
 
-#include <windows.h>
-#define sleep   // Sleep(50)
+#define sleep  //Sleep(80)
 
 typedef struct {
+	int pos;
+	int count;
 	int id;
-	char * buf;
-	int  buf_len;
+	char reply[2];
 }session_data_t;
 
-class server_loop : public sglib::net::iocp_loop<server_loop>
-{
-	friend class sglib::net::iocp_loop<server_loop>;
+#define BUF_LEN  1024*64 // 32k
 
-public:
-	server_loop() {
+class logic_loop : public sglib::net::iocp_loop<logic_loop>
+{
+	FILE * _pf;
+	//char   _buf[BUF_LEN];
+	char  *_buf;
+	int    _piece_size;
+	bool   _quit;
+
+	friend class sglib::net::iocp_loop<logic_loop>;
+
+	int read (session_data_t * s){
+		int len;
+		if(!_pf) 
+			_pf = fopen("test\\server.jpg", "rb");
+		
+		if(_pf){
+			fseek(_pf, s->pos, SEEK_SET);
+			len = (int)fread(_buf, 1, _piece_size, _pf);
+			if(0 == len){
+				s->pos = 0;			
+				s->count ++;
+				DWORD err = GetLastError();
+				return 0;
+			}
+			s->pos = ftell(_pf);	
+		}
+		return len;
 	}
-	~server_loop(){
+	
+public:
+	logic_loop() : _pf(0), _quit(false){
+		_piece_size = BUF_LEN;
+		_buf = new char[BUF_LEN];
+	}
+	~logic_loop(){
+		if(_pf)
+			fclose(_pf);
+		if(_buf) delete []_buf;
 	}
 protected:
 	void on_connected() {
-		std::cout << "\nsession[" << _session->get_id() <<"] NET_connected";
+		int sessionid = _session->get_id() ;
+		std::cout << "\nsession[" << sessionid <<"] NET_connected";
 
 		session_data_t * sdata = new session_data_t;
 		memset(sdata, 0, sizeof(session_data_t));
-		sdata->buf_len = 512;
-		sdata->buf = new char[sdata->buf_len];
-		sdata->id = 0;
-
+		
 		_session->set_session_data(sdata);
-		sprintf(sdata->buf, "session::%d:: string from server NO. %d\n", _session->get_id(), sdata->id );
-		_session->send(sdata->buf, strlen(sdata->buf));
+		sdata->pos = 0;
+		sdata->count = 0;
+		sdata->id = 0;
+		
+		if(sessionid == 1){
+			int foo = 1;
+		}
+		int len = read(sdata);
+		assert(len > 0);
+		_session->send(_buf, len);
 		sleep;
 	}
 
@@ -54,86 +88,93 @@ protected:
 		if (0 == connections())
 			quit_loop(true);
 	}
-
 	void on_send_finished() {
-		int sid = _session->get_id();
-		ASSERT(sid >= 0);
-		std::cout << "\nsession["<< sid <<"] NET_send_finished";
+		std::cout << "\nsession["<< _session->get_id()<<"] NET_send_finished";
 		
 		session_data_t * sdata =(session_data_t *) _session->get_session_data();
-		sdata->id ++;
-		sprintf(sdata->buf, "session::%d:: string from server NO. %d\n", _session->get_id(), sdata->id );
+		_session->recv(sdata->reply, 2);
 		sleep;
-
-		// !!!important!!!!, if we quit, we can't invoke any io operations any more. 
-		if(sdata->id == 300)
-			quit_cur_session(); 
-		else
-			_session->send(sdata->buf, strlen(sdata->buf));
 	}
 	void on_recv_finished() {
 		std::cout << "\nsession["<< _session->get_id()<<"] NET_recv_finished";
+		session_data_t * sdata =(session_data_t *) _session->get_session_data();
+		char * data = sdata->reply;
+		//ASSERT(data == sdata->reply);
+		if(data[0] == 'o' && data[1] == 'k' ) {
+			int len = read(sdata);
+			sdata->id ++;
+			if(len == 0){
+				_buf[0] = '0';
+				len = 1;
+			}
+			_session->send(_buf, len);
+		}
+		else {
+			std::cout << "\nsession["<< _session->get_id()<<"] reply failed";
+		}
 		sleep;
 	}
-	
-	void on_recv_failed(){
-		// buf's size is not enough!
-		session_data_t * sdata = (session_data_t *)_session->get_session_data();
-		int pack_size = _pack.pack_size;
-
-		ASSERT(pack_size > sdata->buf_len);
-		sdata->buf_len = pack_size * 2;
-		delete []sdata->buf;
-		sdata->buf = new char[sdata->buf_len] ;
-		_session->recv(sdata->buf, sdata->buf_len);
-	}
-
-
 	void on_send_error() {
-		std::cout << "\nsession["<< _session->get_id()<<"] NET_send_error: error code = " << _pack.err;
+		std::cout << "\nsession["<< _session->get_id()<<"] NET_send_error";
+		// ***** DO NOT CALL quit here. 
+		//_session->quit();
 		sleep;
 	}
 	void on_recv_error() {
 		std::cout << "\nsession["<< _session->get_id()<<"] NET_recv_error";
+		// ***** DO NOT CALL quit here. 
+		//_session->quit();
 		sleep;
 	}
-
+	void on_recv_failed() {
+		// buf's size is not enough!
+		std::cout << "\nsession["<< _session->get_id()<<"] NET_recv_failed";
+		session_data_t * sdata = (session_data_t *)_session->get_session_data();
+		int pack_size = _pack.pack_size;
+		//ASSERT(pack_size > sdata->buf_len);
+		//sdata->buf_len = pack_size * 2;
+		//delete []sdata->buf;
+		//sdata->buf = new char[sdata->buf_len] ;
+		//_session->recv(sdata->buf, sdata->buf_len);
+	}
 	void on_protocal_error(){
 		std::cout << "\n ******* session["<< _session->get_id()<<"] NET_protocol_error ******";
 		sleep;
 	}
-	void on_noop(){
-	}
-
 	void on_unhandled() {
 		std::cout << "\nsession["<< _session->get_id()<<"] **** NET_unhandled ! ****";		
 	}
+	void on_noop(){
+	}
+
 };
 
 
 int main(int argc, char * argv[])
 {
-	int port = 5001;
-	int client_limit = 5000;
+	int port = 5000;
+	int client_limit = 500;
 	
 	//   create the package queue first. 
-	//   server or clients will create io threads to read or write data from the queue.
+	//   the server or clients will create io threads to read or write the queue.
 	//   the loop also read & remove items from the queue.
-	//   server and client are the producer of the queue
+	//   so the server or client is the producer of the queue
 	//   the loop is the consumer of the queue
-	//   so the queue should be created first and be deleted at last! 
+	//   so the queue should be deleted at last! created first!
 	sglib::net::msg_queue::init();
-	sglib::net::iocp_server _server(port, client_limit);
-	
+	sglib::net::iocp_server _server( port
+								   , client_limit
+								   , BUF_LEN + 64 // send buffer size
+								   , BUF_LEN + 64); // recv buffer size
+
 	int ret = _server.open();
-	if(!ret){  
-		sglib::net::msg_queue::get_singleton()->deref();
+	if(!ret)  
 		return 0;
-	}
 	
+	//_server.listener_loop();
 	_server.thread_listerner();
 
-	server_loop loop;
+	logic_loop loop;
 	loop.init();
 
 	int count = 0 ;
